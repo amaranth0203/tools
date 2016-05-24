b17d8217287168b40ce4c377ace965977b86e658
QiYunhu <357628953@qq.com>
2016-05-24 18:09:06 -0400

    modify to suit

--- sources/rexec.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#192.168.2.39(compiler108)       connected
-#192.168.2.132(compiler213)      connected
-
-cms -i 192.168.2.132 shell "/tmp/... 2"
-cms -i 192.168.2.132 shell "rm -f ~/.bash_history"
